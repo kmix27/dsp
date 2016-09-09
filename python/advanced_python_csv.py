3e693e23246b8a824608143007691838977db2f1
kyle mix <kylemix@kyles-MacBook-Pro.local>
2016-09-09 14:07:58 -0700

    completed part III dictionary, added code to 'advanced_python_dict.py', updated '05b-advanced_python.md' with all answers

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,19 +1,28 @@
 # we will follw the same flow as we did in q4 of regex with pandas again
 import pandas as pd
+import re
+import csv
 # first we'll create a data frame from our csv
 data = pd.read_csv('faculty.csv')
 df = pd.DataFrame(data)
-
+# again we fix the column names
 df.rename(columns={'name': 'name', ' degree': 'degree',
  ' title': 'title', ' email': 'email'}, inplace=True)
-
+# we creat the regular expression to find an email address
 emailFind = '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}'
 
-# with that regex how about a sweet one liner to find all emails,
-# and flatten them into a list. How deliciously pythonic. #thanksWesMcKinny
+# we use that regex with pandas's str.findall method, which finds the emails,
+# and creates a list for each row. we use tolist() on that to encase those in 
+# an overall list, and then we use a list comprehension to flatten our list of lists
 emailList = [item for sublist in df.email.str.findall(emailFind, flags=re.IGNORECASE).tolist() for item in sublist]
 
-# now we will creat a clean data frame to write directly to csv
-dfEmail = pd.DataFrame()
-dfEmail = emailList
-print(dfEmail)
+# we define the filename for the csv we are going to write
+myfile = open('emails.csv', 'w')
+#now we define a writer object poiting to the file we just opened using 
+# the csv module, we define '\n' as a delimiter to put each element on it's own line
+wr = csv.writer(myfile, delimiter='\n')
+# we call our writer with the writerow function and pass our emailList as
+# the parameter which writes our list to our newly created csv 
+wr.writerow(emailList)
+# finally we close our file
+myfile.close()
--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,61 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import re
+from collections import defaultdict
+# first we'll create a data frame from our csv
+data = pd.read_csv('faculty.csv')
+df = pd.DataFrame(data)
+
+df.rename(columns={'name': 'name', ' degree': 'degree',
+ ' title': 'title', ' email': 'email'}, inplace=True)
+
+
+# create a new column of last names to use a dict keys
+df['lastName'] = [item[-1] for item in df['name'].str.strip().str.split(' ')]
+
+# this removes 'of biostatistics'. I tried to do this with .split() and
+# couldn't figure out how to get it back into a string from a list.
+# probably not hard to do, this works for now, but I need to learn that
+df['simpTitle']=[item[:-17] for item in df['title'].str.strip()]
+
+# this merges degree, simpTitle, and email to a list for simplicity later on
+df['merged']= df[['degree', 'simpTitle', 'email']].values.tolist()
+# print(df['merged'])
+
+# I played around with a bunch of metods to creat the dictionary, I kept running
+# into issues surrounding the need for multiple values assigned to one key
+# I found this method on stack exchange and it works well, first it groups
+# the dataframe by lastname using groupby() and assigns that to g.  from there
+# we create the variable facculty_dict and we pass the grouped version of our 
+# merged column to which we apply a lambda function where we turn it into a list
+# we then pass that to the pandas to_dict() function which gives us what we're looking for
+g = df.groupby('lastName')
+faculty_dict = g['merged'].apply(lambda s: s.tolist()).to_dict()
+# print(faculty_dict)
+
+# Finally we use a dictionary comprehension to return the first 3 key value pairs
+# we apply sorted() to the keys so that we can index them, it throws a TypeError
+# without that addition
+firstThree = {k: faculty_dict[k] for k in sorted(faculty_dict.keys())[:3]}
+
+# in order to re-design the keys as 'last name, first name' 
+# first we'll create a column in our dataframe with that format
+df['firstName'] = [item[0] for item in df['name'].str.strip().str.split(' ')]
+# print(df['firstName'])
+
+# combine the last name and first name columns, and make the result a tuple
+df['firstLast'] = df[['firstName','lastName']].apply(tuple, axis=1)
+
+# tuple is seperated into individual letters, creates the dict,  but the key is wrong
+g2 = df.groupby('firstLast')
+professor_dict = g2['merged'].apply(lambda s: s.tolist()).to_dict()
+# print(sorted(df.lastName))
+
+# sorts by first name
+firstThreeProfessor = {k: professor_dict[k] for k in sorted(professor_dict.keys())[:5]}
+
+# print(firstThreeProfessor)
+
+# sorts by last name
+firstThreeByLast = {k: professor_dict[k] for k in sorted(professor_dict.keys(),key=lambda x: x[1])[:3]}
+
+# print(firstThreeByLast)
